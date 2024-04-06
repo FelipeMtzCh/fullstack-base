78d3d1edd905cd423c44612f372e614668975c50
FelipeMtzCh <fmtzcha@gmail.com>
2024-04-06 17:12:21 -0500

    Added trpc server

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TrpcModule } from './trpc/trpc.module';
+import { TrpcService } from './trpc/trpc.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), TrpcModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TrpcService],
 })
 export class AppModule {}
